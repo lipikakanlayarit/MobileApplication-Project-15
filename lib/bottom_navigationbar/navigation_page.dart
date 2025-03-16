45952f723e36fb3536a052b591e84acb4e5f38b2
Thanakit Kirtthanachanpong <Tanakit.kir@student.mahidol.edu>
2025-03-16 19:14:44 +0700

    First time upload

---
+++ lib/bottom_navigationbar/navigation_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:mobile_project/pages/Articles_page.dart';
+import 'package:mobile_project/pages/Profile_page.dart';
+import 'package:mobile_project/pages/Quest_page.dart';
+import 'package:mobile_project/pages/dashboard_page.dart';
+import 'package:mobile_project/pages/home_page.dart';
+
+class BottomNavigationPage extends StatefulWidget {
+  const BottomNavigationPage({super.key});
+
+  @override
+  State<BottomNavigationPage> createState() => _BottomNavigationPageState();
+}
+
+class _BottomNavigationPageState extends State<BottomNavigationPage> {
+
+  int myCurrentIndex = 0;
+  List page = const[
+    HomePage(),
+    ArticlesPage(),
+    QuestPage(),
+    DashboardPage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+
+      bottomNavigationBar: BottomNavigationBar(
+        
+      
+        currentIndex: myCurrentIndex,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.blue,
+
+        onTap: (index){
+          setState(() {
+            myCurrentIndex = index;
+          });
+        },
+        
+        items: const [ 
+        BottomNavigationBarItem(icon: Icon(Icons.home_outlined),label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.newspaper_outlined),label: 'Article'),
+        BottomNavigationBarItem(icon: Icon(Icons.list_alt_outlined),label: 'Quest'),
+        BottomNavigationBarItem(icon: Icon(Icons.bar_chart_outlined),label: 'Dashboard'),
+        BottomNavigationBarItem(icon: Icon(Icons.person_outlined),label: 'Profile'),
+      ],),
+      body: page[myCurrentIndex]
+    );
+  }
+}
---
+++ lib/pages/Articles_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ArticlesPage extends StatelessWidget {
+  const ArticlesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('This is the Article Page'),
+      ),
+    );
+  }
+}
